6462c898c5d51bdc21ec01c1d24f5e5340eaa00b
Scott Walker <scott.walker@puppet.com>
2018-12-04 21:22:52 +0000

    Day 4 solutions

---
+++ cmd/day04b/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+
+	"github.com/scottyw/adventofcode2018/pkg/aoc"
+)
+
+const dim = 1200
+
+func main() {
+	// This implementation assumes the input is sorted, which I did using the editor
+	lines := aoc.FileToLines("input/4.txt")
+	// [1518-03-22 00:00] Guard #503 begins shift
+	// [1518-03-22 00:17] falls asleep
+	// [1518-03-22 00:36] wakes up
+	// [1518-03-23 00:02] Guard #223 begins shift
+	// [1518-03-23 00:23] falls asleep
+	// [1518-03-23 00:55] wakes up
+	guardRegex := regexp.MustCompile("\\[\\d\\d\\d\\d-\\d\\d-\\d\\d \\d\\d:\\d\\d\\] Guard #(\\d+)")
+	sleepsRegex := regexp.MustCompile("\\[\\d\\d\\d\\d-\\d\\d-\\d\\d \\d\\d:(\\d\\d)\\] falls asleep")
+	wakesRegex := regexp.MustCompile("\\[\\d\\d\\d\\d-\\d\\d-\\d\\d \\d\\d:(\\d\\d)\\] wakes up")
+	var guard, sleeps, wakes int
+	var matches []string
+	sleepTracking := map[int]*[60]int{}
+	for _, line := range lines {
+		matches = guardRegex.FindStringSubmatch(line)
+		if len(matches) > 1 {
+			guard = aoc.Atoi(matches[1])
+			continue
+		}
+		matches = sleepsRegex.FindStringSubmatch(line)
+		if len(matches) > 1 {
+			sleeps = aoc.Atoi(matches[1])
+			continue
+		}
+		matches = wakesRegex.FindStringSubmatch(line)
+		if len(matches) > 1 {
+			wakes = aoc.Atoi(matches[1])
+			shift, ok := sleepTracking[guard]
+			if !ok {
+				shift = &[60]int{}
+				sleepTracking[guard] = shift
+			}
+			for min := sleeps; min < wakes; min++ {
+				shift[min]++
+			}
+		}
+	}
+	var chosenGuard, chosenMinute, sleepiestMinuteTotal int
+	for guard, shift := range sleepTracking {
+		for min := 0; min < len(shift); min++ {
+			if shift[min] > sleepiestMinuteTotal {
+				sleepiestMinuteTotal = shift[min]
+				chosenGuard = guard
+				chosenMinute = min
+			}
+		}
+	}
+	fmt.Println(chosenGuard)
+	fmt.Println(chosenMinute)
+	fmt.Println(chosenGuard * chosenMinute)
+}
